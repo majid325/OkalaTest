f42b6296582574c35b10536787ca8ee480c9a903
m.abdollahi.325@gmail.com <m.abdollahi.325@gmail.com>
2024-12-06 12:57:17 +0330

    init

---
+++ tests/Okala.Exchange.UnitTests/Core/CoinAggregate/CoinConstructor.cs
@@ -0,0 +1,22 @@
+﻿using Okala.Exchange.Core.CoinAggregate;
+
+namespace Okala.Exchange.UnitTests.Core.ContributorAggregate;
+
+public class CoinConstructor
+{
+  private readonly string _testSymbol = "BTC";
+  private Coin? _testCoin;
+
+  private Coin CreateContributor()
+  {
+    return  Coin.Create(_testSymbol);
+  }
+
+  [Fact]
+  public void InitializesName()
+  {
+    _testCoin = CreateContributor();
+
+    Assert.Equal(_testSymbol, _testCoin.Symbol);
+  }
+}
